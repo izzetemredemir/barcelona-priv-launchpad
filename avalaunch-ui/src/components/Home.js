53caa10ae530d45c2270dc127f8ad5275f8126ba
Bora <94193278+boraaoz@users.noreply.github.com>
2022-03-26 04:00:41 +0300

    Update Home.js

--- avalaunch-ui/src/components/Home.js
+++ avalaunch-ui/src/components/Home.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./Home.css";
 
 export default class Home extends Component {
   render() {
     return (
-    <div className="Home"> 
+    <div className="Home" > 
+    <img className="bg" src="https://i.imgur.com/gvpOdfY.jpeg" alt=""/>
             <div className="px-4 py-5 my-5 text-center">
-            <img className="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
-            <h1 className="display-5 fw-bold">Home Page</h1>
+            <img className="d-block mx-auto mb-4" src="https://www.linkpicture.com/view.php?img=LPic623e11d235d401747735918" alt="" width="72" height="57"/>
+            <div className='contento'>
+            <img classnName='boba' src='https://i.imgur.com/G5OYSzC.png' alt="" width="300" height="170"/>    <img src='https://i.imgur.com/wX8lUVW.png' alt="" width="16" height="60"  />
+        
+          
+             
+            
             <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+       
+           
+            <p className="lead mb-4">The first protocol on the Avalanche subnet WAGMI, that offers users to deploy their tokens on the permissionless launchpad and investors to join the decentralized fundraising of the tokens. The goal of Babalaunch is to become the go-to hub of Avalanche subnets for the decentralized, permissionless IDO of tokens.</p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+               <div className='button1'>
+                  <button type="button" className="btn btn-primary btn-lg px-4 bg-">Sales</button>
+                </div>
+                <button type="button" className="btn btn-outline-secondary btn-lg px-3 bg-white">Staking</button>
             </div>
             </div>
-        </div>
+            </div>
+        </div>  
     </div>    
     )
   }
